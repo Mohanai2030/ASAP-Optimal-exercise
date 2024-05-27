7bc1e8480439b1b3779d5e084e8dfd3c793205c3
Mohanakrishnan <mohanakrishnanv18@gmail.com>
2024-05-27 09:58:55 +0530

    added appropriate endpoint ping-pong

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('ping/',(req,res)=>{
-  res.send(pong);
+app.get('/ping',(req,res)=>{
+  res.send("pong");
 })
 
 app.listen(3000,()=>{console.log("listening")});
